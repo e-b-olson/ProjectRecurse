8d90faf3330d3c13f1755fd7b3e5fdc2946ac1ad
Sassarara <sassarara@Eriks-MacBook-Pro.local>
2026-06-06 11:35:56 -0700

    chore: remove original project base in preparation for new Next.js base

--- frontend/next/db/migrations/0011_add_skills.sql
+++
@@ -1,8 +0,0 @@
---> statement-breakpoint
-CREATE TABLE "skills" (
-	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
-	"name" varchar(255) NOT NULL,
-	"description" text,
-	"icon_url" text,
-	"created_at" timestamp with time zone DEFAULT now() NOT NULL
-);
